79756dd9ab66181f197f2fcb1491b138685cabfe
Nadia Islam <121532609+isla6645@users.noreply.github.com>
2023-03-17 21:11:03 -0400

    Create lab2.java

---
+++ labs java/lab2.java
@@ -0,0 +1,13 @@
+Public class lab2 {
+Public static void (String args[]){
+if (args != 2){
+throw new IllegalArgumentException("not 2 arguments");
+}
+Int sum = 0;
+Int product = 0;
+sum = args[0] + args[1];
+product = args[0] + args[1];
+System.output.println(args[0]+" + "+args[2]+" = "+sum);
+System.output.println(args[0]+" * "+args[2]+" = "+product);
+ }
+}
