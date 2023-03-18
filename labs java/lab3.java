0b978f1354c969ba757c8b29559303c45a017370
Nadia Islam <121532609+isla6645@users.noreply.github.com>
2023-03-17 21:27:10 -0400

    Update lab3.java

--- labs java/lab3.java
+++ labs java/lab3.java
@@ -1,12 +1,13 @@
 public class lab3 { 
-public static void main(String args[]){ 
-int arr[][] = {{10, 45}, {2, 42, 67, 5}, {3, 21}}; 
-System.out.println(getSumofRow(arr, 1)); //prints the sum of row 1 } 
-private static int getSumOfRow (int [][] myArray, int row) {
-  int sum = 0;
-  for (int i = 0; i < myArray[row].length; i++){
-      sum += myArray[row][i];
-  }
-  return sum;
-} // getSumOfRow 
-} //main 
+  public static void main(String args[]){ 
+    int arr[][] = {{10, 45}, {2, 42, 67, 5}, {3, 21}}; 
+    System.out.println(getSumofRow(arr, 1)); //prints the sum of row 1 } 
+    private static int getSumOfRow (int [][] myArray, int row) {
+      int sum = 0;
+      for (int i = 0; i < myArray[row].length; i++){
+        sum += myArray[row][i];
+      }
+      return sum;
+    } 
+  } 
+}
