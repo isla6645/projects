f3604b919628dde7ca47c5b888e70a8351ac5de1
Nadia Islam <121532609+isla6645@users.noreply.github.com>
2023-03-17 21:25:10 -0400

    Update lab4.java

--- labs java/lab4.java
+++ labs java/lab4.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
 public class lab4 {
-public static void main(String[] args){
-String input = JOptionPane.showInputDialog(null, "Enter a string");
-int count = 0;
-for (int i = 0; i < input.length(); i++) {
-if (Character.isDigit(input.charAt(i))
-count++;
-}
-JOptionPane.showMessageDialog(null, “There are “+count+” characters which are digits”);
+  public static void main(String[] args){
+    String input = JOptionPane.showInputDialog(null, "Enter a string");
+    int count = 0;
+    for (int i = 0; i < input.length(); i++) {
+      if (Character.isDigit(input.charAt(i)){
+          count++;
+      }
+    }
+    JOptionPane.showMessageDialog(null, “There are “+count+” characters which are digits”);
   }
 }
