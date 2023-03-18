9c65b5e2a1fa98adb58204965bd56d05311cb412
Nadia Islam <121532609+isla6645@users.noreply.github.com>
2023-03-17 21:23:31 -0400

    Update lab2.java

--- labs java/lab2.java
+++ labs java/lab2.java
@@ -1,13 +1,13 @@
-Public class lab2 {
-Public static void (String args[]){
-if (args != 2){
-throw new IllegalArgumentException("not 2 arguments");
-}
-Int sum = 0;
-Int product = 0;
-sum = args[0] + args[1];
-product = args[0] + args[1];
-System.output.println(args[0]+" + "+args[2]+" = "+sum);
-System.output.println(args[0]+" * "+args[2]+" = "+product);
+public class lab2 {
+ public static void (String args[]){
+  if (args != 2){
+   throw new IllegalArgumentException("not 2 arguments");
+  }
+  int sum = 0;
+  int product = 0;
+  sum = args[0] + args[1];
+  product = args[0] + args[1];
+  System.output.println(args[0]+" + "+args[2]+" = "+sum);
+  System.output.println(args[0]+" * "+args[2]+" = "+product);
  }
 }
