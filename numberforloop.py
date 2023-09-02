2454f31f9aa8eb7b770e7584d949960da23e235a
Nadia Islam <121532609+isla6645@users.noreply.github.com>
2023-09-02 10:00:34 -0400

    Delete numberforloop.py

--- numberforloop.py
+++
@@ -1,7 +0,0 @@
-numb = (int(input("Enter a number between 5 and 10 inclusive: ")))
-while numb < 5 or numb > 10:
-    numb = (int(input("Enter a number between 5 and 10 inclusive: ")))
-if numb >= 5 or numb <= 10:
-    for i in range(numb):
-        print("*", end = "")
-        
