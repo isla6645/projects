51970b3c82501960e8f8073d815e72cd82fdf706
Nadia Islam <121532609+isla6645@users.noreply.github.com>
2023-09-02 10:00:42 -0400

    Delete pokemonguess.py

--- pokemonguess.py
+++
@@ -1,6 +0,0 @@
-pokemon = 'pikachu'
-guesspoke = input("Guess my favorite pokemon: ")
-while guesspoke != pokemon:
-    guesspoke = input("Guess my favorite pokemon: ")
-if guesspoke == pokemon:
-    print("You guessed it!")
