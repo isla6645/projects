30b034fb7ee4661971579f89dbc67231c9af7320
Nadia Islam <121532609+isla6645@users.noreply.github.com>
2023-09-02 09:59:51 -0400

    Delete firstrepeatedletstr.py

--- firstrepeatedletstr.py
+++
@@ -1,7 +0,0 @@
-input = "programming"
-length = len(input)
-for i in range(0, length):
-    for j in range(i + 1, length):
-        if input[i] == input[j]:
-            print(input[i])
-            exit()
